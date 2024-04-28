1d68b9fe1fb41dee650dbab746358fe403b43687
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-04-29 02:26:31 +0530

    schema is added fro problem

---
+++ src/models/problemModel.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const problemSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'title can not be empty']
+    },
+
+    description: {
+        type: String,
+        required: [true, 'description can not be empty']
+    },
+
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        default: 'easy'
+    },
+
+    testCases: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+
+            output: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    editorial: {
+        type: String
+    }
+});
+
+const Problem = model('Problem', problemSchema);
+
+export default Problem;
