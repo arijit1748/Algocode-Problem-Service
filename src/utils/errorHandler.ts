ec302052e5615a5f62ecc6e54a9138952c4e64bc
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-01 01:47:41 +0530

    All the error logs are succssfully addded

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,11 +1,13 @@
 import { ErrorRequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import logger from '../configs/loggerConfig';
 import BaseError from '../errors/BaseError';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
     if(err instanceof BaseError) {
+        logger.error(err.message);
         return res.status(err.statusCode).json({
             success: false,
             message: err.message,
@@ -14,6 +16,7 @@ const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
         });
     }
 
+    logger.error('Internal Server Error happening for the incoming request');
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
         message: 'Something went wrong!',
