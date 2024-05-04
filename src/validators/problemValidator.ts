c331eeca676e2f320ea90f95ff54ab8a03e8362c
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-04 22:55:53 +0530

    Fix: added logger in validator also

--- src/validators/problemValidator.ts
+++ src/validators/problemValidator.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { ZodSchema } from 'zod';
 
+import logger from '../configs/loggerConfig';
+
 export const validator = (schema: ZodSchema<unknown>) => (req: Request, res: Response, next: NextFunction) => {
     try {
         schema.parse({
@@ -10,9 +12,10 @@ export const validator = (schema: ZodSchema<unknown>) => (req: Request, res: Res
 
         next();
     } catch (error) {
+        logger.error('Invalid request structure is coming');
         return res.status(StatusCodes.BAD_REQUEST).json({
             success: false,
-            message: 'Invalid structure is getting',
+            message: 'Invalid request structure is coming',
             error: error,
             data: {}
         });
